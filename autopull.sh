34c8118aa2edd2ca39f0066313b8d0c32f703b68
Neruthes <i@neruthes.xyz>
2020-11-23 14:37:13 +0000

    Regular update 2020-11-23T11:40:53-00:00

--- autopull.sh
+++ autopull.sh
@@ -3,4 +3,5 @@
 cd /etc/tinc/NTincNet/git
 git pull origin master -f
 rsync -av /etc/tinc/NTincNet/git/hosts/ /etc/tinc/NTincNet/hosts/
-systemctl restart tincd@NTincNet
+#systemctl restart tincd@NTincNet
+kill -1 $(cat /var/run/tinc.NTincNet.pid)
